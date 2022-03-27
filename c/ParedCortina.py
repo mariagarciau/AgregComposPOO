dc2671ec54d5678d79c003b070ecb4d233d7f35f
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2022-03-27 03:18:03 +0200

    c

--- c/ParedCortina.py
+++ c/ParedCortina.py
@@ -1,39 +1,50 @@
-class Pared(str):
-    def z():
-        pass
+class Pared:
+    def __init__(self, orientacion, dimension):
+        self.orientacion = orientacion
+        self.dimension = dimension
+        self.ventanas = []
+    def superficie_cristal(self):
+        superficie = 0
+        for ventana in self.ventanas:
+            superficie += ventana.dimension
+        return superficie
+class Ventana:
+    def __init__(self, pared, tamañoVentana, proteccion):
+        self.pared = pared
+        self.dimension = tamañoVentana
+        self.pared.ventanas.append(self)
+        if proteccion is None:
+            raise Exception("Campo de proteccion obligatorio")
+        else:
+            self.proteccion = proteccion
 
-class Ventana(float):
-    def z():
-        pass
+class Casa:
+    def __init__(self, listaParedes):
+        self.paredes = listaParedes
+    def superficie_acristalada(self):
+        superficie = 0
+        for pared in self.paredes:
+            superficie = superficie + pared.superficieCristal()
+    def superficieNoCristal(self):
+        suma = 0
+        for pared in self.paredes:
+            suma = suma + pared.dimension
+        suma = suma - self.superficieAcristalada()
+        return suma
+class Proteccion:
+    def __init__(self, proteccion):
+        self.proteccion = proteccion
 
-class Casa(list):
-    def z():
-        pass
 
-class ParedCortina(list):
-    def z():
-        pass
-
-# Instanciación de las paredes 
-pared_norte = Pared("NORTE") 
-pared_oeste = Pared("OESTE") 
-pared_sur = Pared("SUR") 
-pared_este = Pared("ESTE") 
-# Instanciación de las ventanas 
-ventana_norte = Ventana(pared_norte, 0.5) 
-ventana_oeste = Ventana(pared_oeste, 1) 
-ventana_sur = Ventana(pared_sur, 2) 
+pared_norte = Pared("NORTE")
+pared_oeste = Pared("OESTE")
+pared_sur = Pared("SUR")
+pared_este = Pared("ESTE")
+ventana_norte = Ventana(pared_norte, 0.5)
+ventana_oeste = Ventana(pared_oeste, 1)
+ventana_sur = Ventana(pared_sur, 2)
 ventana_este = Ventana(pared_este, 1)
-# Instanciación de la casa con las 4 paredes 
 casa = Casa([pared_norte, pared_oeste, pared_sur, pared_este])
 print(casa.superficie_acristalada())
-# >>> 4.5 # 0.5 + 1 + 2 + 1
+print(casa.superficieNoCristal())
 
-casa.paredes[2] = ParedCortina("SUR", 10)
-print(casa.superficie_acristalada())
-
-
-ventana_norte = Ventana(pared_norte, 0.5)
-ventana_norte = Ventana(pared_norte, 0.5, None)
-ventana_norte = Ventana(pared_norte, 0.5, "Persiana")
-print(casa.superficie_acristalada())
